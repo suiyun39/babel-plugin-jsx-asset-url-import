5a7e0725f4f993b35166a79fdc2fe59d34b1d898
NoDocCat <nodoccat@outlook.com>
2023-02-02 15:31:51 +0800

    test: 添加空路径, 外部路径, 绝对路径, Data URL 相关用例

--- tests/fixtures/img-src/code.tsx
+++ tests/fixtures/img-src/code.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
 export const App: React.FC = () => {
-  return <img src="./logo.png" alt="" />
+  return (
+    <article>
+      <img src="" alt="" />
+      <img src="./logo.png" alt="" />
+      <img src="http://example.com/fixtures/logo.png" alt="" />
+      <img src="//example.com/fixtures/logo.png" alt="" />
+      <img src="/fixtures/logo.png" alt="" />
+      <img src="data:image/png;base64,i" alt="" />
+    </article>
+  )
 }
--- tests/fixtures/img-src/snapshot.tsx
+++ tests/fixtures/img-src/snapshot.tsx
@@ -1,5 +1,14 @@
 import _img_src from "./logo.png";
 import React from "react";
 export const App = () => {
-  return <img src={_img_src} alt="" />;
+  return (
+    <article>
+      <img src="" alt="" />
+      <img src={_img_src} alt="" />
+      <img src="http://example.com/fixtures/logo.png" alt="" />
+      <img src="//example.com/fixtures/logo.png" alt="" />
+      <img src="/fixtures/logo.png" alt="" />
+      <img src="data:image/png;base64,i" alt="" />
+    </article>
+  );
 };
