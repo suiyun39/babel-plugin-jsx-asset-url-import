70f8b70b189cf2439bf4502eea5f4343c4cae574
suiyun39 <suiyun39@gmail.com>
2023-12-13 02:20:10 +0800

    build: 不再提供 cjs 格式的产物

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig(options => ({
   entry: ['src/index.ts'],
   platform: 'node',
-  format: ['esm', 'cjs'],
+  format: ['esm'],
   dts: true,
   splitting: false,
   clean: !options.watch,
